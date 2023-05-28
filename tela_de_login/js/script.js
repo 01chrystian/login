e6b794298a1707cbb54d02d0212859f1b81e2e4d
Chrystian do Espirito Santo Nascimento <99342538+01chrystian@users.noreply.github.com>
2023-05-28 05:06:42 -0300

    Update script.js

--- tela_de_login/js/script.js
+++ tela_de_login/js/script.js
@@ -55,7 +55,7 @@ function enviarEmail() {
 
     xhr.open('POST', url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://velvety-chaja-df4fdf.netlify.app/');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://velvety-chaja-df4fdf.netlify.app');
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
